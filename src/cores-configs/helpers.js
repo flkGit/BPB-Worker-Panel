5b42cd40ddd3bd893fad2708f2840b9f6834a26e
fanlikai <26158700+flkGit@users.noreply.github.com>
2024-12-05 14:26:05 +0800

    修改重定向域名

--- src/cores-configs/helpers.js
+++ src/cores-configs/helpers.js
@@ -5,7 +5,7 @@ export async function getConfigAddresses(cleanIPs, enableIPv6) {
     const defaultIPv6 = enableIPv6 ? resolved.ipv6.map((ip) => `[${ip}]`) : []
     return [
         globalThis.hostName,
-        'www.speedtest.net',
+        'ksblog.us.kg',
         ...resolved.ipv4,
         ...defaultIPv6,
         ...(cleanIPs ? cleanIPs.split(',') : [])
